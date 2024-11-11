64b359b38177981bdbef1f2c5c9de381ea27df00
bimoalviana <bimoalviana05@gmail.com>
2024-11-11 21:39:44 +0700

    SOAL_5.PY

---
+++ Kelas_C/SOAL_5.PY
@@ -0,0 +1,11 @@
+# NAMA : BIMO ALVIANA SOPIAN
+# NIM  : 2403010071
+# KELAS:C
+kata = input("Algoritma: ")
+
+indeks_awal = int(input("2: "))
+indeks_akhir = int(input("6: "))
+
+substring = kata[2:6]
+
+print(f"Substring: {gori}")
